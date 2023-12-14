6b38815421340eb3aeac6808911138ced975bf52
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-12-14 15:53:33 +0530

    start

--- store-admin/frontend/src/config/SERVER_URL.js
+++ store-admin/frontend/src/config/SERVER_URL.js
@@ -2,7 +2,7 @@
 // const SERVER_URL = "https://award-store.onrender.com/api"
 
 
-const SERVER_URL = "http://store.intucthrissur.com/api"
+const SERVER_URL = "https://store.intucthrissur.com/api"
 
 
 export default SERVER_URL;
