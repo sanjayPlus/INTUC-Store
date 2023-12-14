e663a9beb322a8b2e7ca8c695ff0a9da52482d7c
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-12-14 14:57:55 +0530

    start

--- frontend/src/config/SERVER_URL.js
+++ frontend/src/config/SERVER_URL.js
@@ -1,6 +1,6 @@
 // const SERVER_URL = "http://localhost:3000/api"
 // const SERVER_URL = "https://award-store.onrender.com/api"
-const SERVER_URL = "http://54.198.70.181/api"
+const SERVER_URL = "http://store.intucthrissur.com/api"
 
 
 export default SERVER_URL;
