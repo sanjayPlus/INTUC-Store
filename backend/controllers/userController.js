46f0307fd96a25a2f9496a872b69893e2b9cf26d
Sanjay9745 <sanjaysanthosh919@gmail.com>
2023-12-22 16:22:36 +0530

    auto login added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -744,7 +744,6 @@ const forgotPassword = async (req, res) => {
 const autoLogin = async (req, res) => {
   try {
     const { email, name, password } = req.body;
-    
     // Check if user exists in the database
     const user = await User.findOne({ email: email });
     
