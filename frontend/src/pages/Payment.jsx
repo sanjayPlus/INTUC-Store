d748be1190ba15a9d636692e403a9147bdb1b78c
sanjayplus <sanjaysanthhoshplusitpark@gmail.com>
2024-01-02 16:14:48 +0530

    hey

--- frontend/src/pages/Payment.jsx
+++ frontend/src/pages/Payment.jsx
@@ -64,7 +64,7 @@ function Payment() {
       },
     }).then((res)=>{
       if(res.status === 200){
-        window.location.href = res.data.url;
+        console.log(res.data);
       }
     }).catch((err)=>{
       console.log(err);
